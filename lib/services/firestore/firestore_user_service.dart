9342ef06afc47f2c55f9d12773c4ce319d0b4bea
Robert Brunhage <robertbrunhage@gmail.com>
2019-05-29 11:06:47 +0200

    Added initial Firestore setup (#1)

---
+++ lib/services/firestore/firestore_user_service.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:water_reminder_app/models/user.dart';
+import 'package:water_reminder_app/services/firestore/firestore_constants.dart';
+
+class FirestoreUserService {
+  static Future<void> checkAndCreateUser() async {
+    final firebaseUser = await FirebaseAuth.instance.currentUser();
+    final userReference = Firestore.instance.collection(FirestoreConstants.userCollection).document(firebaseUser.uid);
+
+    final userDocumentSnapshot = await userReference.get();
+
+    if (userDocumentSnapshot.exists == false) {
+      try {
+        User user = User.temp();
+        await userReference.setData(user.toJson());
+      } catch (e) {
+        print(e);
+        throw e;
+      }
+    }
+  }
+
+  static Future<Stream<DocumentSnapshot>> getUserStream() async {
+    final firebaseUser = await FirebaseAuth.instance.currentUser();
+    final userDocumentStream = Firestore.instance.collection(FirestoreConstants.userCollection).document(firebaseUser.uid).snapshots();
+    return userDocumentStream;
+  }
+
+  static Future<void> updateLastLoggedIn() async {
+    final firebaseUser = await FirebaseAuth.instance.currentUser();
+    Map<String, dynamic> update = Map();
+    update.putIfAbsent(User.lastLoggedInField, () => DateTime.now());
+
+    try {
+      await Firestore.instance.collection(FirestoreConstants.userCollection).document(firebaseUser.uid).updateData(update);
+    } catch (e) {
+      print(e);
+      throw e;
+    }
+  }
+}
--- lib/src/root_page.dart
+++ lib/src/root_page.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:water_reminder_app/models/user.dart';
+import 'package:water_reminder_app/services/firestore/firestore_user_service.dart';
 import 'package:water_reminder_app/src/global_blocs/auth/auth_bloc.dart';
+import 'package:water_reminder_app/src/global_blocs/user_bloc.dart';
 import 'package:water_reminder_app/src/login/login_page.dart';
 
 /// This RootPage will display the home page
@@ -15,9 +18,55 @@ class RootPage extends StatelessWidget {
       stream: auth.onAuthStateChanged,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return HomePage(); // return the homepage
+          return HomePageSetup();
         }
         return LoginPage();
+      },
+    );
+  }
+}
+
+class HomePageSetup extends StatefulWidget {
+  @override
+  _HomePageSetupState createState() => _HomePageSetupState();
+}
+
+class _HomePageSetupState extends State<HomePageSetup> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration.zero, () async {
+      final userBloc = Provider.of<UserBloc>(context);
+
+      await FirestoreUserService.checkAndCreateUser();
+      await userBloc.init();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userBloc = Provider.of<UserBloc>(context);
+    return StreamBuilder<User>(
+      stream: userBloc.outUser,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text(
+                snapshot.error.toString(),
+              ),
+            ),
+          );
+        } else if (snapshot.hasData) {
+          final user = snapshot.data;
+          return HomePage(user: user);
+        }
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
       },
     );
   }
@@ -26,7 +75,10 @@ class RootPage extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({
     Key key,
+    @required this.user,
   }) : super(key: key);
+
+  final User user;
 
   @override
   Widget build(BuildContext context) {
