29eb9b7411650cbc5af03c9268aee7c3cb2d66d0
Admin <robertbrunhage@gmail.com>
2019-06-07 09:01:33 +0200

    added google sign in (only develop devices)

--- lib/src/global_blocs/auth/base_auth.dart
+++ lib/src/global_blocs/auth/base_auth.dart
@@ -3,5 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 abstract class BaseAuth {
   Stream<FirebaseUser> get onAuthStateChanged;
   Future<String> signInAnonymously();
+  Future<String> signInWithGoogle();
+  Future<String> syncWithGoogle();
   Future<void> signOut();
 }
--- lib/src/widgets/buttons/google_sign_in_button.dart
+++ lib/src/widgets/buttons/google_sign_in_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:water_reminder_app/src/global_blocs/auth/auth_bloc.dart';
 
 class GoogleSignInButton extends StatelessWidget {
   const GoogleSignInButton({
@@ -7,10 +9,11 @@ class GoogleSignInButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final auth = Provider.of<AuthBloc>(context);
     return ButtonTheme(
       minWidth: 224,
       child: FlatButton(
-        onPressed: () {},
+        onPressed: auth.signInWithGoogle,
         child: Text('Google Sign in'),
       ),
     );
