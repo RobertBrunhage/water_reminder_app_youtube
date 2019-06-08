a226dfdb76c29b5a4b9de610db91b7921452386b
Robert Brunhage <robertbrunhage@gmail.com>
2019-06-08 10:12:29 +0200

    Feature/google sign in (#6)
    * added google sign in (only develop devices)
    
    * sync with google

--- lib/src/global_blocs/auth/auth_bloc.dart
+++ lib/src/global_blocs/auth/auth_bloc.dart
@@ -1,8 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:water_reminder_app/src/global_blocs/auth/base_auth.dart';
 
 class AuthBloc implements BaseAuth {
   final _firebaseAuth = FirebaseAuth.instance;
+
+  GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [
+      'email',
+      'https://www.googleapis.com/auth/contacts.readonly',
+    ],
+  );
 
   @override
   Future<String> signInAnonymously() async {
@@ -18,5 +26,45 @@ class AuthBloc implements BaseAuth {
   @override
   Stream<FirebaseUser> get onAuthStateChanged {
     return _firebaseAuth.onAuthStateChanged;
+  }
+
+  @override
+  Future<String> signInWithGoogle() async {
+    try {
+      final googleUser = await _googleSignIn.signIn();
+      final googleAuth = await googleUser.authentication;
+
+      final credential = GoogleAuthProvider.getCredential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final user = await _firebaseAuth.signInWithCredential(credential);
+      print("Signed in ${user.uid}");
+      return user.uid;
+    } catch (e) {
+      print(e);
+      throw e;
+    }
+  }
+
+  @override
+  Future<String> syncWithGoogle() async {
+    final anonymousUser = await _firebaseAuth.currentUser();
+    final googleUser = await _googleSignIn.signIn();
+    final googleAuth = await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final user = await anonymousUser.linkWithCredential(credential);
+    return user?.uid;
+  }
+
+  @override
+  Future<FirebaseUser> currentUser() {
+    return _firebaseAuth.currentUser();
   }
 }
--- lib/src/widgets/buttons/google_sign_in_button.dart
+++ lib/src/widgets/buttons/google_sign_in_button.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 class GoogleSignInButton extends StatelessWidget {
   const GoogleSignInButton({
     Key key,
+    @required this.onPressed,
   }) : super(key: key);
+
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ButtonTheme(
       minWidth: 224,
       child: FlatButton(
-        onPressed: () {},
+        onPressed: onPressed,
         child: Text('Google Sign in'),
       ),
     );
